07dda88d61e929c10ea525d6e63428ed0a2d1993
cptdanko <bhuman@yourdiagrams.com>
2016-03-04 11:31:33 +1100

    added an interface for the smugglerService

--- src/com/app/services/SmugglerService.java
+++ src/com/app/services/SmugglerService.java
@@ -1,55 +1,14 @@
 package com.app.services;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
 import com.app.models.Smuggler;
 
-@Path("smuggler")
-public class SmugglerService {
+public interface SmugglerService {
 	
-	private Smuggler hondo = new Smuggler(1, "Hondo","Ohnaka");
-	private Smuggler han = new Smuggler(2, "Han","Solo");
-
-	@GET
-	@Path("all")
-	@Produces(MediaType.APPLICATION_JSON)
-	public List<Smuggler> getAll() {
-		List<Smuggler> smugglers = new ArrayList<Smuggler>();
-		smugglers.add(hondo);
-		smugglers.add(han);
-		return smugglers;
-	}
-	@GET
-	@Path("{id}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public String getSmuggler(@PathParam("id") int id) {
-		if(id == 1){
-			return "Weequay";
-		} else {
-			return "Human";
-		}
-	}
-	@GET
-	@Path("race/{id}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public String smugglerRace(@PathParam("id") int id) {
-		if(id == 1){
-			return "Weequay";
-		} else {
-			return "Human";
-		}
-	}
-	@POST
-	@Path("new")
-	public void addSmuggler(Smuggler smuggler) {
-		System.out.println("new smuggler -> name"+ smuggler.getName()+", lastname:"+smuggler.getLastname());
-	}
+	public List<Smuggler> getAll();
+	
+	public String smugglerRace(int id);
+	
+	public void addSmuggler(Smuggler smuggler);
 }
